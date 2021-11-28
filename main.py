7f90d806b32410b43a8cce6f66c2c34a4c283af7
faris <faris_ddx@hotmail.com>
2021-11-28 07:39:39 +0300

    Add -add Filter

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import shutil
 import os
 import argparse
 
+DIRS_FILTERED = []
+ADD_FILTERED = []
 
 def command_parser():
     parser = argparse.ArgumentParser(
@@ -12,6 +14,12 @@ def command_parser():
 
     parser.add_argument("-d","--dirs", metavar="DIRS", type=str, nargs='+', required=True,
                         help="directory of the copies")
+
+    parser.add_argument("-a", "--add", metavar="DIRS", type=str, nargs='+',
+                        help="adds a folder to an existing source file")
+
+    parser.add_argument("-u", "--update", action="store_true",
+                        help="update existing source file")
 
     parser.add_argument("-p", "--path", action="store_true",
                         help="displays the absolute path of the source file")
@@ -50,14 +58,14 @@ if args.path and os.path.isfile(args.source):
 #removing non directory listing to process reachable and unreachable paths
 def dirs_filter():
     
-    dirs = [dirs for dirs in args.dirs if os.path.isdir(dirs)\
+    dirs = set([dirs for dirs in args.dirs if os.path.isdir(dirs)\
             and os.access(dirs, os.R_OK)\
-            and not is_same_dirs_as_src(dirs)]
+            and not is_same_dirs_as_src(dirs)])
   
     if args.verbose:
         num = 1
         
-        for item in list(set(args.dirs)-set(dirs)):
+        for item in list(set(args.dirs)-dirs):
             if is_same_dirs_as_src(item): 
                 print(f"{num} - \"{item}\" removed - cannot copy to the same directory as the source file")
             elif os.path.isdir(item): 
@@ -71,10 +79,23 @@ def dirs_filter():
     else: 
         if args.dirs != dirs: print("Invalid directories removed")
 
-    return dirs
+    return list(dirs)
 DIRS_FILTERED = dirs_filter()
 
-#copy file to specified folders
+def add_filter():
+    history_file, src_not_in_jfile = src_exists()
+    dirs = []
+    if not src_not_in_jfile:
+        directory_in_hist = [os.path.dirname(dirs) for dirs in list(history_file[SRC].getkeys()]
+        dirs = set([dirs for dirs in args.add if os.path.isdir(dirs)\
+                and os.access(dirs, os.R_OK)\
+                and not is_same_dirs_as_src(dirs)\
+                and dirs not in directory_in_hist])
+    return list(dirs)
+ADD_FILTERED = add_filter()
+DIRS_FILTERED += ADD_FILTERED
+
+#copy file to specified folders in --dirs and --add key arguments
 def src_copy():
     if not args.simulate:
         for directory in DIRS_FILTERED:
@@ -88,14 +109,12 @@ def src_copy():
             num += 1
     elif args.quiet: pass
     else: print(f"File {BASE}, Copied Sucessfuly")
-    
-    return DIRS_FILTERED
 
 ### Json ###
 import json
 
 #returns source_history.json if exists, else creates new one
-def src_jfile_get():
+def src_history_jfile_get():
     #stackoverflow.com/a/35249327
     src_hist_dir = os.path.expanduser("~") + ".config/fupdate"
     src_hist_file = src_hist_dir + "/source_history.json"
@@ -105,7 +124,7 @@ def src_jfile_get():
             return source_history
 		
     except FileNotFoundError: 
-        #stackoverflow.com/a/35249327
+        #stackoverflow.com/a/35249327, if you don't copy from sof, what use of you?
         if not os.path.isfile(src_hist_file):
             os.makedirs(src_hist_dir, exist_ok=True)
         
@@ -115,30 +134,48 @@ def src_jfile_get():
         with open(src_hist_file, 'x+', encoding='utf-8') as j_file:
             empty = {}
             json.dump(empty, j_file, indent = 4)
-            return src_jfile_get()
-            
+            return src_history_jfile_get()
 
-
+#uses the src hash and copy time to the destinion copies 
+#for future integrity or update check
 from hashlib import sha1
-def dirs_file_hash(file):
+from time import ctime
+def src_file_hash_time(file):
     with open(file, 'rb') as file:
-        return sha1(file.read()).hexdigest
+        return sha1(file.read()).hexdigest(), ctime()
+
+def src_exists():
+    history_file  = src_history_jfile_get()
+    not_in_jfile = SRC not in history_file
+
+    return  history_file, not_in_jfile
 
 #if src does not exist in source_history.json, 
 #try to create new src and dir list and add to the json file
-def src_create():
-    source_file  = src_jfile_get()
-   
-    for dir_path in DIRS_FILTERED:
-        file_path = os.path.join(dir_path, SRC)
-        
-        if os.path.isfile(file_path):
-            file_hash = dirs_file_hash(file_path)
-        
-    source_file.update(SRC=[file_hash, ])  
-    pass
-
 #if src exists in source_history.json, 
 #try to update existing folders and or add new ones if added through -a flag
-def src_update(src):
-    pass
+def src_update():
+    history_file, not_in_jfile  = src_exists()
+
+    if not_in_jfile:
+        history_file[SRC] = {} 
+    
+    file_hash, file_time = src_file_hash_time(SRC)
+
+    #adds the folders
+    if not_in_jfile:
+        for dir_path in DIRS_FILTERED:
+            file_path = os.path.join(dir_path, SRC)
+            history_file[SRC].update({file_path: [file_hash, file_time]})
+    #updates the folders and add new if flagged
+    else:
+        dirs_existing = list(history_file[SRC].getkeys())
+        dirs_existing_and_added = ADD_FILTERED + dirs_existing
+        
+        for dir_path in dirs_existing_and_added :
+            file_path = os.path.join(dir_path, SRC)
+            history_file[SRC].update({file_path: [file_hash, file_time]})
+    
+    src_copy()
+    
+    return history_file
